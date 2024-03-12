be8ab7ac63d08b47eb0c75b70f49b57abdf841f1
Kerry Guarino <kerry.guarino@gmail.com>
2024-03-12 17:00:25 -0400

    able to sumbit info in form on front end and post to back end

--- client/src/components/SquadForm.jsx
+++ client/src/components/SquadForm.jsx
@@ -7,35 +7,21 @@ function SquadForm({addSquad}){
    const [description, setDescription] = useState("")
    const [image, setImage] = useState("")
    
-   // THIS NEEDS TO BE TURNED INTO A POST TO THE BACKEND
-   // function handleForm(e){
-   //    e.preventDefault()
-   //    let newSquad = {
-   //       name: name,
-   //       description: description,
-   //       image: image
-   //    }
-   //    addSquad(newSquad)
-   //    setName("")
-   //    setImage("")
-   //    setDescription("")
-   // }
-
    function handleSubmit(e) {
       e.preventDefault();
       fetch("http://127.0.0.1:5555/squads", {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify({
-          name: name,
-          image: image,
-          description: description,
-        }),
+         method: "POST",
+         headers: {
+            "Content-Type": "application/json",
+         },
+         body: JSON.stringify({
+            name: name,
+            image: image,
+            description: description,
+         }),
       })
-        .then((r) => r.json())
-        .then((newSquad) => addSquad(newSquad));
+         .then((r) => r.json())
+         .then((newSquad) => addSquad(newSquad));
     }
 
 
