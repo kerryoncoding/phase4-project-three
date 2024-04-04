d569d4d47860767d45c2cd0087aca5d130254014
Kerry Guarino <kerry.guarino@gmail.com>
2024-04-03 20:01:20 -0400

    added formik to new message form

--- client/src/components/FeedNewMessage.jsx
+++ client/src/components/FeedNewMessage.jsx
@@ -5,13 +5,6 @@ import * as yup from "yup";
 
 function FeedNewMessage({ makePosting, member, leaveSquad, joinSquad }) {
 
-   const [body, setBody] = useState("");
-   
-   function handleSubmit(e) {
-      e.preventDefault();
-      makePosting(body)
-      setBody("");
-   }
 
    function handleLeaveClick() {
       leaveSquad()
@@ -21,18 +14,36 @@ function FeedNewMessage({ makePosting, member, leaveSquad, joinSquad }) {
       joinSquad()
    }
 
+   // Formik
+   const formSchema = yup.object().shape({
+      body: yup.string().required("Field can not be left blank").max(125, "Exceeds max length"),
+   });
 
+   const formik = useFormik({
+      initialValues: {
+        body: "",
+      },
+      validationSchema: formSchema,
+      onSubmit: (values, {resetForm}) => {
+         makePosting(values.body)
+         resetForm()
+      },
+   });
+   
+
+   
    if (member) {
       return (
          <>
-         <form className="new-message" onSubmit={handleSubmit}>
+         <form className="new-message" onSubmit={formik.handleSubmit}>
             <input
                type="text"
                name="body"
-               value={body}
-               onChange={(e) => setBody(e.target.value)}
+               onChange={formik.handleChange}
+               value={formik.values.body}
                placeholder="   new message..."
-            />
+               />
+               <p style={{ color: "red" }}> {formik.errors.body}</p>
             <button className="messageToggleButton" type="submit">Send</button>
          </form>
          <button onClick={handleLeaveClick} className="messageToggleButton" type="submit">Leave this Squad</button>
