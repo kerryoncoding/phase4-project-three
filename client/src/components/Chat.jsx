692e07de5e613c2bc9a15c57ef092031aef33795
Kerry Guarino <kerry.guarino@gmail.com>
2024-05-11 13:49:27 -0400

    working on layout for chatroom

--- client/src/components/Chat.jsx
+++ client/src/components/Chat.jsx
@@ -13,9 +13,14 @@ function Chat() {
             <br/>
             <div className="card-container">
 
-               <h1>Welcome to the Chatroom!</h1>
-               <hr className="breakline" />
-               <h3>TBD</h3>
+               <h1>Welcome to Live Chat!</h1>
+               {/* <hr className="breakline" /> */}
+               {/* <h3>TBD</h3> */}
+               <div className="chatbox"></div>
+               <form>
+                 <h3>Message:<input></input><button className="messageToggleButton">send</button></h3>
+               </form>
+               
             </div>
          </div>
       </div>
