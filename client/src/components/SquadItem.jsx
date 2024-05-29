b6ebcb3936bef0ddb9e89e703d074e1e820522d2
Kerry Guarino <kerry.guarino@gmail.com>
2024-05-29 19:58:27 -0400

    continued to clean out commented code

--- client/src/components/SquadItem.jsx
+++ client/src/components/SquadItem.jsx
@@ -15,7 +15,6 @@ function SquadItem({ user, active, item, deleteCard, toggleView }) {
 		return (
 			<div className="card" >
 				<button className="cardDeleteButton" onClick={handleDeleteClick}>✖</button>
-				{/* <button className="cardDeleteButton" onClick={handleDeleteClick}>✅</button> */}
 				<h3>{item.name}</h3>
 				<img src={item.image} className="card-image" alt={item.name}></img>
 				<p>{item.description}</p>
@@ -27,8 +26,6 @@ function SquadItem({ user, active, item, deleteCard, toggleView }) {
 		return (
 			<div className="card" >
 				<button className="cardDeleteButtonGone">✖</button>
-				{/* <button className="cardDeleteButton" >O</button> */}
-				{/* <button className="cardDeleteButton" >✅</button> */}
 				<h3>{item.name}</h3>
 				<img src={item.image} className="card-image" alt={item.name}></img>
 				<p>{item.description}</p>
